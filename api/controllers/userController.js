eb80398e0148e3736a20131b73328cfa4e0d1e65
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-06-22 22:34:46 +0200

    created api for deleting favChannel

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,169 +1,193 @@
 const sqlite3 = require("sqlite3");
 const Encrypt = require("../Encrypt");
-const path = require('path');
+const path = require("path");
 const fetch = require("node-fetch");
+const { query } = require("express");
 
-const db = new sqlite3.Database(path.join(__dirname , "../../RadioSwedenDB.db"));
+const db = new sqlite3.Database(path.join(__dirname, "../../RadioSwedenDB.db"));
 
 //here we check which session is active and return this to the frontend
 //. If I want user just see some part I have to use this part of cod
 const whoami = (req, res) => {
-    res.json(req.session.user || null);
+  res.json(req.session.user || null);
 };
 
 const login = (req, res) => {
-    // the person can log in here
-    let query = /*sql*/ `SELECT * FROM users WHERE email = $email`;
-    let params = {$email: req.body.email};
+  // the person can log in here
+  let query = /*sql*/ `SELECT * FROM users WHERE email = $email`;
+  let params = { $email: req.body.email };
 
-    db.get(query,params, (err, userInDB) => {
-        if(!userInDB) {
-            res.status(401).json({error: "Bad credentials!!!"});
-            return;
-        }
-        // comparing passwords
-        req.body.password = Encrypt.encrypt(req.body.password);
-        if (userInDB.password === req.body.password) {
-            delete userInDB.password;
-            req.session.user = userInDB;
-            res.json({success: "Login successfull", loggedInUser: userInDB});
-            return;
-        } else {
-            res.status(401).json({error: "Bad credentials" });
-            return;
-        }
-    });
+  db.get(query, params, (err, userInDB) => {
+    if (!userInDB) {
+      res.status(401).json({ error: "Bad credentials!!!" });
+      return;
+    }
+    // comparing passwords
+    req.body.password = Encrypt.encrypt(req.body.password);
+    if (userInDB.password === req.body.password) {
+      delete userInDB.password;
+      req.session.user = userInDB;
+      res.json({ success: "Login successfull", loggedInUser: userInDB });
+      return;
+    } else {
+      res.status(401).json({ error: "Bad credentials" });
+      return;
+    }
+  });
 };
 
 const addToChannelFavoriteList = async (req, res) => {
+  if (!req.session.user) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
 
-    if(!req.session.user)
-    {
-        res.status(401).json({ error: "Unauthorized"});
-        return;
+  const channelId = req.params.channelId;
+  const userId = req.session.user.id;
+
+  let query = /*sql*/ `INSERT INTO userChannels (userId, channelId) VALUES ($userId, $channelId)`;
+  params = {
+    $userId: userId,
+    $channelId: channelId,
+  };
+
+  db.run(query, params, function (err) {
+    if (err) {
+      res.status(400).json({ error: err });
+      return;
     }
 
-    const channelId = req.params.channelId;
-    const userId = req.session.user.id;
-
-
-    query = /*sql*/ `INSERT INTO userChannels (userId, channelId) VALUES ($userId, $channelId)`;
-    params = { 
-        $userId: userId, 
-        $channelId: channelId, 
-    };
-
-    db.run(query, params, function(err){
-        if (err) {
-            res.status(400).json({error: err});
-            return;
-        }
-
-        res.json({Success: "Channel has been added to your favorites", lastID: this.lastID});
-        return;
+    res.json({
+      Success: "Channel has been added to your favorites",
+      lastID: this.lastID,
     });
+    return;
+  });
 };
 
 const getFavoriteChannels = async (req, res) => {
+  if (!req.session.user) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
 
-    if(!req.session.user)
-    {
-        res.status(401).json({ error:"Unauthorized"});
-        return;
+  const userId = req.session.user.id;
+
+  let query = /*sql*/ `SELECT * FROM userChannels WHERE userId = $userId`;
+  params = {
+    $userId: userId,
+  };
+
+  db.all(query, params, (err, channels) => {
+    if (err) {
+      res.status(400).json({ error: err });
+      return;
     }
 
-    const userId = req.session.user.id;
+    res.json(channels);
+    return;
+  });
+};
 
-    query = /*sql*/ `SELECT * FROM userChannels WHERE userId = $userId`;
-    params = {
-        $userId: userId
+const deleteFromChannelFavoriteList = async (req, res) => {
+  const channelId = req.params.channelId;
+
+  let query = /*sql*/ `DELETE FROM userChannels WHERE channelId = $channelId`;
+  params = {
+    $channelId: channelId,
+  };
+
+  db.run(query, params, (err, channels) => {
+    if (err) {
+      res.status(400).json({ error: err });
+      return;
     }
 
-    db.all(query, params, (err, channels) => {
-        if (err) {
-            res.status(400).json({error: err});
-            return;
-        }
-
-        res.json(channels)
-
-
-    })
-}
-
-
-
-
-
-
+    res.json({
+      Success: "Channel has been deleted successfully",
+      channels,
+    });
+    return;
+  });
+};
 
 const addToProgramFavoriteList = async (req, res) => {
+  if (!req.session.user) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
 
-    if(!req.session.user)
-    {
-        res.status(401).json({ error: "Unauthorized"});
-        return;
+  const programId = req.params.programId;
+  const userId = req.session.user.id;
+
+  query = /*sql*/ `INSERT INTO userPrograms (userId, programId) VALUES ($userId, $programId)`;
+  params = {
+    $userId: userId,
+    $programId: programId,
+  };
+
+  db.run(query, params, function (err) {
+    if (err) {
+      res.status(400).json({ error: err });
+      return;
     }
 
-    const programId = req.params.programId;
-    const userId = req.session.user.id;
-
-
-    query = /*sql*/ `INSERT INTO userPrograms (userId, programId) VALUES ($userId, $programId)`;
-    params = { 
-        $userId: userId, 
-        $programId: programId, 
-    };
-
-    db.run(query, params, function(err){
-        if (err) {
-            res.status(400).json({error: err});
-            return;
-        }
-
-        res.json({Success: "Program has been added to your favorites", lastID: this.lastID});
-        return;
+    res.json({
+      Success: "Program has been added to your favorites",
+      lastID: this.lastID,
     });
+    return;
+  });
 };
 
 const logout = (req, res) => {
-    delete req.session.user;
-    res.json({ success: true});
+  delete req.session.user;
+  res.json({ success: true });
 };
 
 const register = (req, res) => {
-    let userToRegister = req.body; 
-    
-    // to check if the user already exists
-    let query = /*sql*/`SELECT * FROM users WHERE email = $email`;
-    let params = {$email: userToRegister.email};
-    db.get(query, params, (err, userExist) => {
-        if(userExist) {
-            res.status(400).json({error: "User with that email already exists"});
-            return;
-        }
+  let userToRegister = req.body;
 
-        userToRegister.password = Encrypt.encrypt(userToRegister.password);
-        query = /*sql*/ `INSERT INTO users (firstName, lastName, email, password) VALUES ($firstName, $lastName, $email, $password)`;
-        params = { 
-            $firstName: userToRegister.firstName , 
-            $lastName: userToRegister.lastName, 
-            $email: userToRegister.email, 
-            $password: userToRegister.password 
-        };
-    
-        db.run(query, params, function(err){
-            if (err) {
-                res.status(400).json({error: err});
-                return;
-            }
-    
-            res.json({Success: "User registered successfully", lastID: this.lastID});
-            return;
-        });
+  // to check if the user already exists
+  let query = /*sql*/ `SELECT * FROM users WHERE email = $email`;
+  let params = { $email: userToRegister.email };
+  db.get(query, params, (err, userExist) => {
+    if (userExist) {
+      res.status(400).json({ error: "User with that email already exists" });
+      return;
+    }
+
+    userToRegister.password = Encrypt.encrypt(userToRegister.password);
+    query = /*sql*/ `INSERT INTO users (firstName, lastName, email, password) VALUES ($firstName, $lastName, $email, $password)`;
+    params = {
+      $firstName: userToRegister.firstName,
+      $lastName: userToRegister.lastName,
+      $email: userToRegister.email,
+      $password: userToRegister.password,
+    };
+
+    db.run(query, params, function (err) {
+      if (err) {
+        res.status(400).json({ error: err });
+        return;
+      }
+
+      res.json({
+        Success: "User registered successfully",
+        lastID: this.lastID,
+      });
+      return;
     });
-
+  });
 };
 
-
-module.exports = { whoami, login, logout, register, addToChannelFavoriteList, addToProgramFavoriteList, getFavoriteChannels};
+module.exports = {
+  whoami,
+  login,
+  logout,
+  register,
+  addToChannelFavoriteList,
+  addToProgramFavoriteList,
+  getFavoriteChannels,
+  deleteFromChannelFavoriteList,
+};
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -6,7 +6,9 @@ router.get("/whoami", userController.whoami);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 router.post("/register", userController.register);
+router.get("/getFavoriteChannels", userController.getFavoriteChannels);
 router.post("/addFavoriteChannel/:channelId", userController.addToChannelFavoriteList);
+router.delete("/deleteFavoriteChannel/:channelId", userController.deleteFromChannelFavoriteList);
 router.post("/addFavoriteProgram/:programId", userController.addToProgramFavoriteList);
 
 
