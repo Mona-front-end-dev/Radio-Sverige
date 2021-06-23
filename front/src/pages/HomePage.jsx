d0089a5af9368ce22e72a9bdc9411c7da01de147
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-06-23 19:44:37 +0200

    fixed isInFavorite, call get FaveChannels under useEffect

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -3,21 +3,29 @@ import { StationContext } from "../contexts/StationProvider"
 
 import styles from "../css/HomePage.module.css";
 import ChannelItem from "../components/ChannelItem";
+import { FavoriteContext } from "../contexts/FavoritContext";
 
 const HomePage = () => {
     const { channels, getAllChannels} = useContext(StationContext);
+    const { favoriteChannels, getFavoriteChannels} = useContext(FavoriteContext);
    
     
     useEffect(() => {
         getAllChannels();
+        getFavoriteChannels();
     }, []);
 
     let channelView;
     if(channels)
-        channelView = channels.map(channel => <ChannelItem channel={channel} isInFavorite={true} /> );
+        channelView = channels.map(channel => {
+            
+            const isInFavorite = favoriteChannels.find(fc => fc.channelId === channel.id);
+
+            return <ChannelItem channel={channel} isInFavorite={isInFavorite} /> 
+        });
     else
         channelView = null;
-
+    // console.log(channelView);
     return (
         <div className={styles.channelsList}>
             <h1>All channels are listed below</h1>
