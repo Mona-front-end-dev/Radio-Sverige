6653515f23b248319f705a7d609e3695e43d6aef
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-06-26 20:20:19 +0200

    render faveBtns in favorites page, it works well

--- front/src/pages/FavoritePage.jsx
+++ front/src/pages/FavoritePage.jsx
@@ -3,22 +3,45 @@ import { StationContext } from "../contexts/StationProvider";
 import { FavoriteContext } from "../contexts/FavoritContext";
 import styles from "../css/ProgramPage.module.css";
 import ChannelItem from "../components/ChannelItem";
+import ProgramItem from "../components/ProgramItem";
 
-const FavoritePage = (props) => {
-  const { channels, getAllChannels } = useContext(StationContext);
-  const { favoriteChannels, getFavoriteChannels } = useContext(FavoriteContext);
+const FavoritePage = () => {
+  const { channels, getAllChannels, programs, getAllPrograms } =
+    useContext(StationContext);
+  const {
+    favoriteChannels,
+    getFavoriteChannels,
+    getFavoritePrograms,
+    favoritePrograms,
+  } = useContext(FavoriteContext);
 
   useEffect(() => {
     getAllChannels();
+    getAllPrograms();
     getFavoriteChannels();
+    getFavoritePrograms();
   }, []);
 
   const renderFavoriteChannels = () => {
     return channels
-      .filter(c => favoriteChannels.find(fc => fc.channelId === c.id))
-      .map(c => <ChannelItem channel={c} isInFavorite={true} />);
+      .filter((c) => favoriteChannels.find((fc) => fc.channelId === c.id))
+      .map((c) => <ChannelItem channel={c} isInFavorite={true} />);
   };
-  return <div>{renderFavoriteChannels()}</div>;
+
+  const renderFavoritePrograms = () => {
+    return programs
+    .filter((p) => favoritePrograms.find((fp) => fp.programId === p.id))
+    .map((p) => <ProgramItem program={p} isInFavorite={true} />);
+  };
+
+  return (
+    <div>
+      <h1>Your Channels</h1>
+      <div>{renderFavoriteChannels()}</div>
+      <h1>Your Programs</h1>
+      <div>{renderFavoritePrograms()}</div>
+    </div>
+  );
 };
 
 export default FavoritePage;
