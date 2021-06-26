4e8af2332d6a6d714628a559f18ca2ffdf57089a
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-06-26 15:12:45 +0200

    update to render add/Remove FaveProgram Btn, as well as PItem

--- front/src/pages/ProgramsPage.jsx
+++ front/src/pages/ProgramsPage.jsx
@@ -2,34 +2,50 @@ import { useEffect, useContext } from "react";
 import { useHistory } from "react-router-dom";
 
 import { StationContext } from "../contexts/StationProvider";
-import styles from "../css/ProgramsPage.module.css"
+import styles from "../css/ProgramPage.module.css";
+import { FavoriteContext } from "../contexts/FavoritContext";
+import ProgramItem from "../components/ProgramItem" ;
 
 const ProgramsPage = (props) => {
-    const history = useHistory();
-    const { getProgramsByChannelId, programsByChannelId } = useContext(StationContext);
-    const { channelId } = props.match.params;
+  const history = useHistory();
+  const { getProgramsByChannelId, programsByChannelId } =
+    useContext(StationContext);
+  const { getFavoritePrograms, favoritePrograms } = useContext(FavoriteContext);
+  const { channelId } = props.match.params;
 
-    useEffect(() => {
-        getProgramsByChannelId(channelId);
-    }, []);
+  useEffect(() => {
+    getProgramsByChannelId(channelId);
+    getFavoritePrograms();
+  }, []);
 
-    const clickHandler = (programId) => {
-        history.push(`/programs/${programId}`);
-    };
+  const clickInfoHandler = (programId) => {
+    history.push(`/programs/${programId}`);
+  };
 
-    let content = <h2>Loading..</h2>
-    
-    if(programsByChannelId) {
+  let content = <h2>Loading..</h2>;
 
-        content =  programsByChannelId.map(p =>
-            <div className={styles.card} key={p.id} onClick={() => clickHandler(p.id)}>
-                <div className={styles.title}>
-                    <h2>Program Name: {p.name} </h2>
-                </div>
-            </div>
-        );
-    };
-    return <div>{content}</div>;
+const generateProgram = (program) => {
+    const isInFavorite = favoritePrograms.find(fp => fp.programId === program.id);
+    return <ProgramItem program={program} isInFavorite={isInFavorite} /> 
+
+};
+
+
+
+  if (programsByChannelId) {
+    content = programsByChannelId.map((p) => (
+      <div className={styles.card} key={p.id}>
+        <div className={styles.title}>
+          <h2>Program Name: {p.name} </h2>
+        </div>
+        <div className={styles.flex}>
+          <button onClick={() => clickInfoHandler(p.id)}>Info</button>
+          {generateProgram(p)}
+        </div>
+      </div>
+    ));
+  }
+  return <div>{content}</div>;
 };
 
 export default ProgramsPage;
