b8b1f488c1d39ad4a40aa2fba8a702084df75539
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-06-27 18:08:43 +0200

    moved moreInfo to programItem

--- front/src/pages/ProgramsPage.jsx
+++ front/src/pages/ProgramsPage.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useContext } from "react";
-import { useHistory } from "react-router-dom";
 
 import { StationContext } from "../contexts/StationProvider";
 import styles from "../css/ProgramPage.module.css";
 import { FavoriteContext } from "../contexts/FavoritContext";
-import ProgramItem from "../components/ProgramItem" ;
+import ProgramItem from "../components/ProgramItem";
 
 const ProgramsPage = (props) => {
-  const history = useHistory();
   const { getProgramsByChannelId, programsByChannelId } =
     useContext(StationContext);
   const { getFavoritePrograms, favoritePrograms } = useContext(FavoriteContext);
@@ -18,30 +16,19 @@ const ProgramsPage = (props) => {
     getFavoritePrograms();
   }, []);
 
-  const clickInfoHandler = (programId) => {
-    history.push(`/programs/${programId}`);
-  };
-
   let content = <h2>Loading..</h2>;
 
-const generateProgram = (program) => {
-    const isInFavorite = favoritePrograms.find(fp => fp.programId === program.id);
-    return <ProgramItem program={program} isInFavorite={isInFavorite} /> 
-
-};
-
-
+  const generateProgram = (program) => {
+    const isInFavorite = favoritePrograms.find(
+      (fp) => fp.programId === program.id
+    );
+    return <ProgramItem program={program} isInFavorite={isInFavorite} />;
+  };
 
   if (programsByChannelId) {
     content = programsByChannelId.map((p) => (
-      <div className={styles.card} key={p.id}>
-        <div className={styles.title}>
-          <h2>Program Name: {p.name} </h2>
-        </div>
-        <div className={styles.flex}>
-          <button onClick={() => clickInfoHandler(p.id)}>Info</button>
-          {generateProgram(p)}
-        </div>
+      <div key={p.id}>
+        <div className={styles.flex}>{generateProgram(p)}</div>
       </div>
     ));
   }
